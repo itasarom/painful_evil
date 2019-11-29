ba1a6b1f96359956916c0732b03d2f3423930159
Мазаев Павел Антонович <edu-cmc-skmodel19-617-10@polus-ib.bmc.hpc.cs.msu.ru>
2019-11-29 22:59:19 +0300

    Started building the operator

--- prog.cpp
+++ prog.cpp
@@ -24,6 +24,38 @@ int ComputeLatticeId(int i, int j) {
 	return lattice_m * i + j;
 }
 
+void ComputeLatticeCoord(int id, int* i, int* j) {
+	*j = id % lattice_m;
+	*i = id / lattice_n;
+}
+
+void GetLatticeParams() {
+	lattice_n = 1;
+	lattice_m = 1;
+}
+
+void ComputeBlockSize(int N, int M, int* block_h, int* block_w) {
+	*block_h = N;
+	*block_w = M;
+}
+
+class LocalOperator {
+public:
+	const int N, M;
+protected:
+	int row_pos, col_pos;
+	int block_h, block_w;
+public:
+	LocalOperator(int my_i, int my_j, int block_h_, int block_w_, int N_, int M_):
+		N(N_), M(M_)
+	 {
+		row_pos = my_i;
+		col_pos = my_j;
+		block_h = block_h_;
+		block_w = block_w_;
+	 }
+};
+
 void SendBorder(const vector<double>& border, int dst, int me) {
 	MPI_Send(border.data(), border.size(), MPI_DOUBLE, dst, me, MPI_COMM_WORLD);
 }
@@ -74,16 +106,32 @@ Block Step(const Block& w, int block_row, int block_col, int width, int height) 
 }
 
 */
+
+void ParseArgs(int argc, char **argv, int* N, int* M) {
+	*N = 10;
+	*M = 10;
+}
+
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	std::cout << world_rank << endl;
+	int N, M;
+	ParseArgs(argc, argv, &N, &M);
+
+	GetLatticeParams();
+
+	int my_i, my_j;
+	int block_h, block_w;
 	
-	/*	
-	InitLocalOperator();
+	ComputeLatticeCoord(world_rank, &my_i, &my_j);
+	ComputeBlockSize(N, M, &block_h, &block_w);
+	
+	LocalOperator op(my_i, my_j, N, M, block_h, block_w);
+	
+	/*
 	auto my_w = Solve();
 	SendLocalResultsTo0(my_w);
 	
